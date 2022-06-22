1727876f1fbb5f52585b7c0b9e773444835c7159
Amin <facklak@mail.ru>
2022-06-22 12:41:19 +0300

    init: start Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) { // если число отрицательное то умножаем на -1...да да это колхоз
+        return Number(Array.from((n*-1).toString()).reverse().join(''));
+    } else {
+        return Number(Array.from(n.toString()).reverse().join(''));
+    }
+  // делает число строкой, затем строку в массив, переворачивает массив, делает из массива строку и удаляет запятые, и последнее преобразование из строки в число
 }
